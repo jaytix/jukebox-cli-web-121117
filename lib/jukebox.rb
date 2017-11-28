bd412c6100eaee0b9178121a443daa63068f9369
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 23:17:46 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,8 +34,8 @@ def play(songs)
     if input == song || input.to_i == (index + 1)
       puts "Playing #{song}"
     end
-    puts "Invalid input, please try again"
   end
+  puts "Invalid input, please try again"
 end
 
 def exit_jukebox
