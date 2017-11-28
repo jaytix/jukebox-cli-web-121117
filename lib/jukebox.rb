00f7577403da83552dea017a589dbe58044a9efa
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 23:15:24 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,6 @@ def play(songs)
   songs.each_with_index do |song, index|
     puts "Please enter a song name or number:"
     input = gets.chomp
-    binding.pry
     if input == song || input.to_i == (index + 1)
       puts "Playing #{song}"
     end
